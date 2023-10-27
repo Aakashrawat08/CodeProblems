d88715fc2b2c88489c4d490fc7c32333f2e17b55
aakash <aakash96710@gmail.com>
2023-10-27 23:51:58 +0530

    operatorscode

---
+++ operators.js
@@ -0,0 +1,56 @@
+// divide operator
+
+// var x=8;
+// var y=2;
+// var  z=8/2;
+// console.log(z)
+
+// increment operator
+
+//  let x=5
+//   let y=x--
+//  console.log(y)
+//  console.log(x)
+
+
+// const num3=null
+// console.log(typeof num3)
+
+
+// const fun1= function(){}
+// console.log(typeof fun1)
+
+
+
+//  let x=5
+//  let y= 3
+//  console.log('x+y =',x+y)
+// console.log(++x)
+// console.log(x++)
+// console.log(x)
+
+
+// let x=2+true
+// console.log(x)
+
+// let x='x'*10
+// console.log(x)
+// console.log( typeof x)
+
+
+// let x=5>>0
+// console.log(x)
+// var x=5/"yes"
+// console.log(x)
+// console.log( typeof x)
+// var x=5/"yes"
+// console.log(x)
+// console.log( typeof x)
+// let a=3;
+// let b=new number(3);
+// let c=3
+// console.log(a==b);
+// console.log(a===b);
+// console,log(a===c);
+const sum=eval('10*10+5')
+console.log(sum)
