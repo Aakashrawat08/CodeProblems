c43b1e9be89684398824bfbd4e2a07389274c792
aakash <aakash96710@gmail.com>
2023-10-30 22:14:59 +0530

    firstjs filecommit

---
+++ first.js
@@ -0,0 +1,81 @@
+// console.log("hello world")
+//  var arr=[3,4,6,7]
+//  var sum=0
+// for(i=0;i<arr.length;i++){
+//     sum=sum+arr[i]
+// }
+// console.log(sum)
+// a=5
+// b=2
+// c=6
+// d=8
+// f=1
+// if(a>b&&a>c&&a>d&&a>f){
+//     console.log(a)
+// }
+// else if(b>a&&b>c&&b>d&&b>f){
+//     console.log(b)
+// }
+// else if(c>a&&c>b&&c>d&&c>f){
+//     console.log(c)
+// }
+// else if(d>a&&d>b&&d>c&&d>f){
+//   console.log(d)
+// }
+// else if(f>a&&f>b&&f>c&&f>d)
+// {console.log(f)}
+// else{
+//     console.log("all staements is false")
+}
+//    var a=10
+//    {
+//     var a=-10
+//    }
+//    let b=a;
+//    {
+//     let b=-20
+//    }
+//    console.log(b)
+// for(var i=0;i<=10;i++){
+//     console.log(i)
+// }
+// console.log("i'm outside the var for loop:", i)
+// console.log(i)
+// for(let j=0;j<3;j++){
+//     console.log(j)
+// }
+// console.log("i'm outside the var for loop:", j)
+// var sum=0
+// for(var i=0;i<=100;i++){
+
+    
+// }
+// console.log(i)
+// console.log(y)
+// y=10
+// console.log(typeof('5'+5))
+// let a=5
+// if(a>10){
+//     console.log("if statement is true")
+// }
+// else if(a>20){
+//     console.log("else if first stement true")
+// }
+// else if(a>3){
+//     console.log("else if second stement true")
+// }
+// else if(a>20){
+//     console.log("else if first stement true")
+// }
+// else if(a>40){
+//     console.log("else if third stement true")
+// }
+// else{ console.log("all avove statements is false this is else statement output")
+// }
+// const sum=eval('10*10+5')
+// console.log(sum)
+function sum(a,b){
+    return a+b
+}
+sum(1,'2')
+console.log(sum)
