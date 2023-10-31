c2c732b85747271be70c78822645fee4c41f9778
aakash <aakash96710@gmail.com>
2023-10-31 21:32:33 +0530

    araay problems commit

---
+++ array.js
@@ -0,0 +1,48 @@
+// var arr1=[1,3,4,65,6]
+// var arr2=[2,9,41,5,56]
+//  var arr3=arr1+arr2
+//  console.log(arr3)
+// var arr1=[1,3,4,65,6]
+// var arr2=[2,9,41,5,56]
+//  var arr3=arr1.concat(arr2)
+//  console.log(arr3)
+// var arr1=[1,3,4,65,6]
+// arr1.pop()
+// console.log(arr1)
+// console.log(arr1.length)
+// console.log(arr1[4])
+// arr1.shift()
+// console.log(arr1)
+// var arr1=[1,30,40,65,6]
+// // var finalarray=arr1.find(element => element>10)
+// var sakshi=arr1.findIndex((element)=>{
+//     return element>10
+// })
+// console.log(sakshi)
+// var arr=[3,5,7,9,7,30]
+// var findevery=(element) => element<20
+// console.log(arr.some(findevery))
+// var arr=['kajal','tannu','mainsha','sandhya']
+//  finalarray=arr.reverse()
+// console.log(finalarray)
+// var arr=[2,4,5,6,7,8]
+// arr.forEach(element => console.log(element+20))
+// function digitcount(n){
+//     var count=0
+//     if(n>=1)
+//     ++count;
+// while(n/10>=1){
+//     n/=10;
+//     ++count
+// }
+// return count
+// }
+// console.log(digitcount(1211024))
+// const obj={a:'one',b:'two',a:'three'}
+// console.log(obj)
+let a=3
+let b=new Number(3)
+let c=3
+console.log(a==b)
+console.log(a===b)
+console,log(a===c)
